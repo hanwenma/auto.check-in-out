38e9b13ccf90da67210de212b45137eeb5bf8b58
v_hanwma <v_hanwma@tencent.com>
2024-03-15 11:45:20 +0800

    fix: 掘金变量重复声明报错

--- juejin.js
+++ juejin.js
@@ -1,9 +1,10 @@
-const commomDelay = 3000;
-const submitDelay = 5000;
-const KeyTexts = ["去签到", "立即签到"];
+(async function init() {
+  const commomDelay = 3000;
+  const submitDelay = 5000;
+  const KeyTexts = ["去签到", "立即签到"];
 
-function logExtensionInfo() {
-  const text = `
+  function logExtensionInfo() {
+    const text = `
           M          M           M    M M M M M M M         M                         M       M  M
          M M         M           M          M            M      M                     M     M      M
         M   M        M           M          M          M          M                   M      M
@@ -12,96 +13,96 @@ function logExtensionInfo() {
      M         M      M         M           M            M      M              M      M    M      M
     M           M       M  M  M             M               M           M        M  M        M  M
     `;
-  console.log(
-    `%c${text}`,
-    `background: linear-gradient(to right, purple, red, orange, yellow,green, cyan, blue, purple, red, orange, yellow,green, cyan, blue, purple );
+    console.log(
+      `%c${text}`,
+      `background: linear-gradient(to right, purple, red, orange, yellow,green, cyan, blue, purple, red, orange, yellow,green, cyan, blue, purple );
             background-size: 100%;
             background-clip: text;
             -webkit-background-clip: text;
             color: transparent;
             text-shadow: 0 5px 10px #fff;
             font-weight: bold;`
-  );
+    );
 
-  console.log(
-    "%cPerform relevant operations after 3 seconds !",
-    `
+    console.log(
+      "%cPerform relevant operations after 3 seconds !",
+      `
   color: blue;
   font-size: 20px;
   font-weight: bold;`
-  );
-}
+    );
+  }
 
-function sleep(delay) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, delay);
-  });
-}
+  function sleep(delay) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, delay);
+    });
+  }
 
-async function start() {
-  await sleep(commomDelay);
-  $(".signin-btn").click(); // 点击去签到按钮
+  async function start() {
+    await sleep(commomDelay);
+    $(".signin-btn").click(); // 点击去签到按钮
 
-  await sleep(commomDelay);
-  $(".signin.btn").click(); // 点击立即签到按钮
+    await sleep(commomDelay);
+    $(".signin.btn").click(); // 点击立即签到按钮
 
-  await sleep(commomDelay);
-  $(".btn-area .btn").click(); // 点击去抽奖
+    await sleep(commomDelay);
+    $(".btn-area .btn").click(); // 点击去抽奖
 
-  await sleep(commomDelay);
-  $("#turntable-item-0").click(); // 点击免费抽奖
+    await sleep(commomDelay);
+    $("#turntable-item-0").click(); // 点击免费抽奖
 
-  await sleep(submitDelay);
-  $(".submit").click(); // 点击收下奖励
+    await sleep(submitDelay);
+    $(".submit").click(); // 点击收下奖励
 
-  prepareNextChectIn();// 准备下一次签到
-}
+    prepareNextChectIn(); // 准备下一次签到
+  }
 
-function prepareNextChectIn() {
-  const hours = moment(moment().format("YYYY-MM-DD") + " 07:00")
-    .add(1, "days")
-    .diff(moment(), "hours");
+  function prepareNextChectIn() {
+    const hours = moment(moment().format("YYYY-MM-DD") + " 07:00")
+      .add(1, "days")
+      .diff(moment(), "hours");
 
-  console.log(
-    `%cYou will be automatically checked in after【 ${hours} 】hour!`,
-    `color: #02cb0f;
+    console.log(
+      `%cYou will be automatically checked in after【 ${hours} 】hour!`,
+      `color: #02cb0f;
      font-size: 20px;
      font-weight: bold;`
-  );
+    );
 
-  setTimeout(() => {
-    location.href = "https://juejin.cn";// 跳转到签到页
-  }, hours * 60 * 60 * 1000);
-}
-
-function autoCheckIn() {
-  const GoCkITetx = $(".signin-btn").text().trim();
-  const ImCkIText = $(".signin.btn").text().trim();
-
-  switch (GoCkITetx || ImCkIText) {
-    case KeyTexts[0]:
-      // 点击去签到按钮
-      $(".signin-btn").click();
-      break;
-
-    case KeyTexts[1]:
-      // 正式开始签到
-      start();
-      break;
-
-    default:
-      // 准备下次签到
-      prepareNextChectIn();
-      break;
+    setTimeout(() => {
+      location.href = "https://juejin.cn"; // 跳转到签到页
+    }, hours * 60 * 60 * 1000);
   }
-}
 
-(function init() {
+  function autoCheckIn() {
+    const GoCkITetx = $(".signin-btn").text().trim();
+    const ImCkIText = $(".signin.btn").text().trim();
+
+    switch (GoCkITetx || ImCkIText) {
+      case KeyTexts[0]:
+        // 点击去签到按钮
+        $(".signin-btn").click();
+        break;
+
+      case KeyTexts[1]:
+        // 正式开始签到
+        start();
+        break;
+
+      default:
+        // 准备下次签到
+        prepareNextChectIn();
+        break;
+    }
+  }
+  
   // 输出提示
   logExtensionInfo();
 
   // 签入
+  await sleep(commomDelay);
   autoCheckIn();
-})()
+})();
